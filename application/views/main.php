22de04f56ad735df0a70b68eec8fc870ec6acd3e
Antonioch1no <jesus.chino907@gmail.com>
2023-04-20 19:11:34 -0600

    prueba

---
+++ application/views/main.php
@@ -0,0 +1,130 @@
+<!-- Comienza a escribir tu código aquí -->
+
+
+<!-- Borra el siguiente código: -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Nombre de la página</title>
+  <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css'); ?>">
+  <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
+  <script src="<?php echo base_url('assets/js/jquery.min.js'); ?>"></script>
+  <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
+    <meta charset="utf-8">
+  <title>Prueba Técnica - Frontend</title>
+  <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
+  <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="<?= base_url(); ?>assets/css/custom_bootstrap.css">
+  <link rel="stylesheet" href="<?= base_url(); ?>assets/css/components.css">
+</head>
+<body>
+  <header>
+    
+  </header>
+  
+  <main>
+  <!DOCTYPE html>
+<html lang="es">
+<head>
+  <title>Mi Página Web</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+  <header>
+  </header>
+  
+  <main>
+  <div class="container mt-5 mb-5">
+  <div >
+  <div class="row">
+    <div class="col">
+      <img src="./assets/images/Frame1.png" class="img-fluid mx-auto d-block" alt="">
+    </div>
+  </div>
+</div>
+
+
+<div class="row mt-5 mb-0">
+  <div class="col">
+    <h2>Subtitulo 1</h2>
+    <p>Lorem ipsum</p>
+  </div>
+  <div class="col">
+    <h2>Subtitulo 2</h2>
+    <p>Lorem ipsum</p>
+  </div>
+  <div class="col">
+    <h2>Subtitulo 3</h2>
+    <p>Lorem ipsum</p>
+  </div>
+</div>
+
+<div class="mt-5 mb-5">
+  <div class="d-flex justify-content-center">
+    <img src="./assets/images/Frame3.png" class="img-fluid w-100" alt="">
+  </div>
+</div>
+
+<div class="mt-5 mb-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 d-flex align-items-center">
+        <div class="circle">1</div>
+        <h2>Subtitulo 1</h2>
+      </div>
+      <div class="col-md-6 d-flex align-items-center">
+        <div class="circle">2</div>
+        <h2>Subtitulo 2</h2>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  .circle {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    background-color: #FFD25F;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    font-size: 24px;
+    margin-right: 10px;
+  }
+</style>
+
+
+</div>
+      
+  </main>
+  
+  <footer >
+    <div class="bg-grayLight">
+  <div class="container">
+  <div class="row">
+    <div class="col-md-6">
+      <h2>Sítio</h2>
+      <p>Inicio</p>
+      <p>Nosotros</p>
+      <p>Preguntas Frecuentes</p>
+    </div>
+    <div class="col-md-6">
+      <h2>Contácto</h2>
+      <p>12345678</p>
+      <p>mail@gmail.com</p>
+    </div>
+  </div>
+</div>
+</div>
+  </footer>
+</body>
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="es-MX">
+
+<head>
+  <meta charset="utf-8">
+  <title>Prueba Técnica - Frontend</title>
+  <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
+  <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="<?= base_url(); ?>assets/css/custom_bootstrap.css">
+  <link rel="stylesheet" href="<?= base_url(); ?>assets/css/components.css">
+</head>
+
+<body>
+  <div class="py-3rem px-1rem5 bg-grayLight">
+    <h1 class="font-weight-bold">Centro de Seguros - Grupo Promass</h1>
+    <h3 class="mt-1rem">Prueba técnica - Frontend</h3>
+  </div>
